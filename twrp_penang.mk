ed809f519d1fa8986e293e3f2b07a09bb303eb93
Jamie Falconer <mrfluffyoven@gmail.com>
2024-08-19 09:09:24 +0100

    Initial commit

---
+++ twrp_penang.mk
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from penang device
+$(call inherit-product, device/motorola/penang/device.mk)
+
+PRODUCT_DEVICE := penang
+PRODUCT_NAME := twrp_penang
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := Holi for arm64
+PRODUCT_MANUFACTURER := motorola
+
+PRODUCT_GMS_CLIENTID_BASE := android-motorola
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="penang_g-user 11 U1TPS34.29-83-6-1 f01ec release-keys"
+
+BUILD_FINGERPRINT := motorola/penang_g/penang:11/U1TPS34.29-83-6-1/f01ec:user/release-keys
